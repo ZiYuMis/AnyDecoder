88a68b02b8e92584992ba5b994aaf20ef4ef1b64
ZiYuMis <zizhidiyu@163.com>
2021-05-21 16:46:22 +0800

    helloworld

---
+++ build/CMakeFiles/AnyDecoder.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AnyDecoder"
+  "AnyDecoder.pdb"
+  "CMakeFiles/AnyDecoder.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AnyDecoder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
